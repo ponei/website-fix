bfdb161f41023ec0610d813e66b63dff4292d1b8
ponei <poneialt@gmail.com>
2021-06-30 23:13:41 -0300

    Create brainly.user.js

---
+++ brainly.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Brainly Bypass
+// @id         Brainly Bypass
+// @version      1.0
+// @author       ponei
+// @namespace    brainly.bypass
+// @include      http*://brainly.com*/tarefa/*
+// @icon         https://styleguide.brainly.com.br/images/favicons/brainly/favicon-0c2222f36b.ico
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(mut => {
+        if (mut.addedNodes.length) {
+            mut.addedNodes.forEach(mutNode => {
+                if (mutNode.nodeName == "SCRIPT") {
+                    if (mutNode.src.includes("__qpage")) {
+                        mutNode.src = "";
+                        observer.disconnect();
+                    }
+                }
+            })
+        }
+    })
+});
+observer.observe(document, {childList: true, subtree: true});
